5046357ac30406b20b1b07533150f789012ad8cb
olivermchaleTU <olivermchale98@gmail.com>
2020-02-08 23:24:38 +0000

    Add models for database setup

---
+++ StoryService.Models/Dtos/StoryDto.cs
@@ -0,0 +1,19 @@
+﻿using StoryService.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StoryService.Models.Dtos
+{
+    public class StoryDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public Guid CreatedBy { get; set; }
+        public List<Guid> Assignees { get; set; }
+        public DateTime DueBy { get; set; }
+        public Guid SuperStoryId { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
